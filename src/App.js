aef8916fe28235219201972c0578bafb6ef1d33f
Gehaime Barros <{ID}+{gehaimeBarros}@users.noreply.github.com>
2024-10-07 17:38:58 -0300

    REQ000093901021ST

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import CreateRoom from './components/CreateRoom';
 import Room from './components/Room';
 import AuthExample from './components/AuthExample';
@@ -24,7 +24,7 @@ function App() {
   }
 
   return (
-    <Router basename="/opensecurityroom">
+    <Router>
       <Routes>
         <Route path="/login" element={<AuthExample />} />
         <Route path="/" element={user ? <CreateRoom /> : <Navigate to="/login" />} />
